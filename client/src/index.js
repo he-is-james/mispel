038218ff466d194b908add8947092048832b3177
James <he.is.james7@gmail.com>
2022-06-30 21:15:36 -0400

    created frontend routing

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
-//import Landing from './pages/Landing';
-//import Roomcode from './pages/Roomcode'
+import Landing from './pages/Landing';
+import Roomcode from './pages/Roomcode';
 import Setup from './pages/Setup'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Setup />
-  </React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="create-room" element={<Setup />} />
+        <Route path="join-room" element={<Roomcode />} />
+      </Routes>
+    </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function

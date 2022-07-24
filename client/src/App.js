0f2d29e935af3a8ec8c292ca911278e62bb6405d
Kevin Nguyen <ktnguy20@gmail.com>
2022-07-24 19:29:55 -0400

    test frontend to database connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import audioCtx from './index';
+import axios from 'axios';
 
 function App() {
+  const handleClick = () => {
+    axios.get('/api/getSound').then((res) => {
+      const source = audioCtx.createBufferSource();
+      source.buffer = res.data;
+      source.connect(audioCtx.destination);
+      source.start();
+    });
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +20,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick = {handleClick}>
+          Play Sound
+        </button>
       </header>
     </div>
   );

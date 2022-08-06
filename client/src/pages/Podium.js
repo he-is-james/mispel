49d16675446157c725644e1c20254fa4a253adef
akshayd2020 <63755431+akshayd2020@users.noreply.github.com>
2022-08-06 19:03:05 -0400

    Update Podium.js

--- client/src/pages/Podium.js
+++ client/src/pages/Podium.js
@@ -7,14 +7,14 @@ function Podium() {
 		<div className="flex flex-col items-center justify-center bg-orange font-rubikone text-center text-white min-h-screen">
 			<label className="text-9xl mt-12">Congratulations!</label>
 			<div>#standings here</div>
-			<div className="flex flex-row flex-wrap justify-content mt-auto mb-12 w-[90%] mt-12">
-				<div className="flex grow justify-start">
+			<div className="flex flex-row items-center flex-nowrap justify-content mt-auto mb-12 w-[90%] mt-12 min-w-fit">
+				<div className="flex justify-start mr-10">
 					<Options />
 				</div>
-				<div className="flex grow justify-center">
-					<label className="text-7xl text-center ml-20">Your Score: 11000</label>
+				<div className="flex grow justify-center whitespace-nowrap">
+					<label className="text-7xl text-center">Your Score: 11000</label>
 				</div>
-				<div className="flex grow justify-end">
+				<div className="flex justify-end flex whitespace-nowrap">
 					<button className="bg-navy text-5xl py-4 px-6 rounded-md hover:bg-sky">Play Again</button>
 				</div>
 			</div>

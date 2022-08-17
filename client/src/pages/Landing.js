13a1bc97bfc2e3d6f58272da64a4ab791437f4b3
Kevin <ktnguy20@gmail.com>
2022-08-16 23:56:59 -0400

    Revert "refactor routing, initial links in landing"
    This reverts commit c53af8f75b58f98404349f40c543873c6d136c59.

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 
 function Landing() {
   return (
@@ -10,13 +9,9 @@ function Landing() {
         </label>
         <input type="text" name="name" className="text-center text-3xl bg-gray-400 mt-6 w-[75%] h-12 rounded-md focus:outline-none"/>
       </div>
-      <Link to="join-room">
-        <button className="bg-navy text-4xl py-2 px-10 mt-8 rounded-md hover:bg-sky">Join Room</button>
-      </Link>
+      <button className="bg-navy text-4xl py-2 px-10 mt-8 rounded-md hover:bg-sky">Join Room</button>
       <br/>
-      <Link to="create-room">
-        <button className="bg-navy text-4xl py-2 px-7 mt-2 rounded-md hover:bg-sky">Create Room</button>
-      </Link>
+      <button className="bg-navy text-4xl py-2 px-7 mt-2 rounded-md hover:bg-sky">Create Room</button>
     </div>
   );
 }
